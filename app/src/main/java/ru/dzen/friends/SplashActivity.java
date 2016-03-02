887d483eceebcf03e831f3fd307be2d78f349d57
andrewRybakin <drnraw@yandex.ru>
2016-03-02 17:58:49 +0400

    Wrong activity launching error fix

--- app/src/main/java/ru/dzen/friends/SplashActivity.java
+++ app/src/main/java/ru/dzen/friends/SplashActivity.java
@@ -61,7 +61,7 @@ public class SplashActivity extends AppCompatActivity {
                                             .setCancelable(false)
                                             .show();
                                 } else {
-                                    Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                                    Intent i = new Intent(SplashActivity.this, LoginActivity.class);
                                     startActivity(i);
                                     finish();
                                 }
